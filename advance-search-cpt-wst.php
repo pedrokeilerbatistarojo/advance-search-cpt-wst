bd801b23cfb29211a66492af62a29727463d05ff
laravel777crew <pedro@777crew.com>
2024-10-07 11:17:37 -0500

    First commit

---
+++ advance-search-cpt-wst.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Dynamic Cpt Work and Study Travel
+Plugin URI: https://github.com/pedrokeilerbatistarojo/advance-search-cpt-wst
+Description: Advance search for CPT and Custom fields WST website
+Version: 1.0
+Author: Pedro Keiler Batista Rojo <pedrokeilerbatistarojo>
+Author URI: https://github.com/pedrokeilerbatistarojo
+*/
+
